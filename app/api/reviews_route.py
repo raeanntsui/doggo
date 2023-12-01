04ddcfa798b77c220759db12c272c3348a02a683
Raeann <raeann.tsui@gmail.com>
2023-12-01 15:20:05 -0500

    read and create reviews done

---
+++ app/api/reviews_route.py
@@ -0,0 +1,80 @@
+from flask import Blueprint, jsonify, redirect, request
+from flask_login import login_required, current_user
+from app.models import db, Review, User
+from app.forms import ReviewForm
+# from .auth_routes import validation_errors_to_error_messages
+
+
+reviews_route = Blueprint('reviews', __name__)
+
+@reviews_route.route("/<int:product_id>")
+def product_reviews(product_id):
+    '''
+    Get all reviews for a specific product
+    '''
+    reviews = Review.query.filter_by(product_id=product_id).all()
+    return jsonify([review.to_dict() for review in reviews])
+
+@reviews_route.route("/new/<int:product_id>", methods=["POST"])
+@login_required
+def create_review(product_id):
+    '''
+    Create a new review for a specific product
+    '''
+    form = ReviewForm(request.form)
+    form['csrf_token'].data = request.cookies['csrf_token']
+    print(form.data)
+    if form.validate_on_submit():
+
+        # review_description = form.review_description.data
+        # rating = form.rating.data
+        # review_image =form.review_image.data
+
+        rating = form.data["rating"]
+        review_description = form.data["review_description"]
+        review_image = form.data["review_image"]
+
+        new_review = Review(product_id=product_id, review_description=review_description, rating=rating, review_image=review_image, user_id=current_user.id)
+
+        db.session.add(new_review)
+        db.session.commit()
+
+        return {"newReview": new_review.to_dict()}
+    else:
+
+        errors = form.errors
+        return jsonify({"message": "Invalid form submission", "errors": errors}), 400
+
+@reviews_route.route("/<int:review_id>", methods=["PUT"])
+@login_required
+def update_review(review_id):
+    '''
+    update an existing review
+    '''
+    form = ReviewForm(request.form)
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        review = Review.query.get(review_id)
+
+        if not review:
+            return (jsonify({"error": "Review not found"}), 404)
+
+        review.description = form.description.data
+        review.rating = form.rating.data
+
+        db.session.commit()
+
+        return {"updateReview": review.to_dict()}
+    else:
+        return (jsonify({"error": "Invalid form data"}), 400)
+
+@reviews_route.route('/<int:review_id>', methods=['DELETE'])
+def delete_review(review_id):
+    review = Review.query.get(review_id)
+    if review:
+        db.session.delete(review)
+        db.session.commit()
+        return jsonify({"message": "Review deleted successfully"})
+    else:
+        return jsonify({"error": "Review not found"}), 404
