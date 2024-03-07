a905dbba1735070e61899b916b2758b98e9b065c
Raeann <raeann.tsui@gmail.com>
2024-03-06 22:25:23 -0800

    adding search bar -- not yet complete

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,11 +1,21 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
+  const [searchInput, setSearchInput] = useState("");
+  const history = useHistory();
   const sessionUser = useSelector((state) => state.session.user);
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    history.push(`/products?search=${encodeURIComponent(searchInput)}`);
+  };
 
   return (
     <>
@@ -14,6 +24,20 @@ function Navigation({ isLoaded }) {
           <NavLink exact to="/">
             <h1 id="dogsy-title">Dogsy</h1>
           </NavLink>
+        </div>
+        <div className="searchbar">
+          <form onSubmit={handleSearchSubmit}>
+            <input
+              className="searchbar-input"
+              type="text"
+              value={searchInput}
+              onChange={handleSearchInputChange}
+              placeholder="Search for anything"
+            />
+            <button type="submit">
+              <i className="fas fa-search" style={{ color: "white" }}></i>
+            </button>
+          </form>
         </div>
         <div id="right-side-nav">
           <NavLink id="all-listings-nav-bar" exact to="/products">
